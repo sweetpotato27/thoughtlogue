a318e496bc461d7927705da41c3c2fa6c1860cc2
sweetpotato27 <matthewsdb34@gmail.com>
2021-04-15 14:08:28 -0700

    login req

--- app.js
+++ app.js
@@ -3,12 +3,22 @@ const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
 const port = process.env.PORT || 5000;
+const bodyParser = require('body-parser');
+
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 
 mongoose
-    .connect(db, { useNewUrlParser: true })
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello Dylan"));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
