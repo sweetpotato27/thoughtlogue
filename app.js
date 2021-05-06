df4b2b094169de074f70c692c50583a52b261769
sweetpotato27 <matthewsdb34@gmail.com>
2021-05-05 17:30:08 -0700

    attempt to start apollo server

--- app.js
+++ app.js
@@ -1,34 +1,50 @@
 const mongoose = require('mongoose');
 const express = require("express");
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schema');
 const passport = require('passport');
-const app = express();
 const db = require('./config/keys').mongoURI;
 const path = require('path');
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  })
+async function startApolloServer() {
+  const app = express();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  await server.start();
+
+  server.applyMiddleware({ app });
+  
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+  }
+  
+  const port = process.env.PORT || 5000;
+  
+  const users = require("./routes/api/users");
+  const thoughts = require("./routes/api/thoughts");
+  
+  
+  mongoose
+      .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+      .then(() => console.log("Connected to MongoDB successfully"))
+      .catch(err => console.log(err));
+  
+  app.use(passport.initialize());
+  require('./config/passport')(passport);
+  
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+  
+  app.use("/api/users", users);
+  app.use("/api/thoughts", thoughts);
+  
+  // app.listen(port, () => console.log(`Server is running on port ${port}`));
+  await new Promise(resolve => app.listen({ port: port}, resolve));
+  console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`);
+  return { server, app };
 }
-
-const port = process.env.PORT || 5000;
-
-const users = require("./routes/api/users");
-const thoughts = require("./routes/api/thoughts");
-
-mongoose
-    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("Connected to MongoDB successfully"))
-    .catch(err => console.log(err));
-
-app.use(passport.initialize());
-require('./config/passport')(passport);
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-app.use("/api/users", users);
-app.use("/api/thoughts", thoughts);
-
-app.listen(port, () => console.log(`Server is running on port ${port}`));
---
+++ index.js
@@ -0,0 +1,52 @@
+/* GRAPHQL SECTION */
+const { ApolloServer, gql } = require('apollo-server');
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = gql`
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Thought" type defines the queryable fields for every thought in our data source.
+  type Thought {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "thoughts" query returns an array of zero or more Thoughts (defined above).
+  type Query {
+    thoughts: [Thought]
+  }
+`;
+
+const thoughts = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+const resolvers = {
+  Query: {
+    thoughts: () => thoughts,
+  },
+};
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
+
+/*************     */
